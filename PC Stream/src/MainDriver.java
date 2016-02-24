0f38e5dcf7738f0089a677d70dd818f4119c11d1
Gregory Grzymski <ggrzymski@gmail.com>
2016-02-23 22:29:21 -0500

    starting template on streaming app.

---
+++ PC Stream/src/MainDriver.java
@@ -0,0 +1,21 @@
+
+/**
+ * I want to implement a streaming connection for a NVIDIA-based gpu. SHIELD integrates the hardware encoder
+ * on the card and sends the signal to the Geforce Experience application to launch the desired application.
+ * I would like to dig deeper into SHIELD's code to see if I could do it on the Windows platform, which hasn't
+ * been released yet under the NVIDIA domain. This would prove beneficial for laptop/surface users who don't
+ * want to invest in the Android environment, and it would provide a more comprehensive experience for individuals
+ * who have to take their gaming on the go.
+ * 
+ * @author Gregory Grzymski
+ *
+ */
+public class MainDriver 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
